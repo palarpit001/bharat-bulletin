c92aaee97cb167870de5a031a988819118823593
palarpit001 <palarpit970@gmail.com>
2026-07-14 16:51:32 +0530

    Fix business page module error

---
+++ app/business/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function BusinessPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Business News</h1>
+      <p>Business page content coming soon...</p>
+    </div>
+  );
+}
