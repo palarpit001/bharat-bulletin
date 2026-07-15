678904e9d7e3a73a7b40d7ca2e0c5452ce5ebf2a
palarpit001 <palarpit970@gmail.com>
2026-07-15 10:38:06 +0530

    Dynamic SEO

--- app/article/[slug]/page.tsx
+++ app/article/[slug]/page.tsx
@@ -1,5 +1,37 @@
+import { Metadata } from "next";
 import ArticleClient from "../../components/ArticleClient";
 
-export default function Page() {
+type Props = {
+  params: Promise<{ slug: string }>;
+};
+
+export async function generateMetadata({
+  params,
+}: Props): Promise<Metadata> {
+  const { slug } = await params;
+
+  const title = slug
+    .replace(/-/g, " ")
+    .replace(/\b\w/g, (c) => c.toUpperCase());
+
+  return {
+    title: `${title} | Bharat Bulletin`,
+    description: `Read the latest news about ${title} only on Bharat Bulletin.`,
+    openGraph: {
+      title: `${title} | Bharat Bulletin`,
+      description: `Latest news and updates about ${title}.`,
+      url: `https://https://bharat-bulletin-lilac.vercel.app//article/${slug}`,
+      siteName: "Bharat Bulletin",
+      type: "article",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `${title} | Bharat Bulletin`,
+      description: `Latest news about ${title}`,
+    },
+  };
+}
+
+export default async function Page() {
   return <ArticleClient />;
 }
